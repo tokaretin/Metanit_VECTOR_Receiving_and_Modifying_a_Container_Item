573802658658eb99e0be769a67e72657b7f933d8
tokaretin <tokaretin@gmail.com>
2023-05-26 16:24:41 +0200

    Добавьте файлы проекта.

---
+++ Metanit_VECTOR_Receiving_and_Modifying_a_Container_Item/Metanit_VECTOR_Receiving_and_Modifying_a_Container_Item.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+
+    std::cout << "Получение и изменение элемента контейнера" << '\n';
+    std::vector<int> vec{ 1,2,3,4,5,6 };
+    for (int n : vec)
+    {
+        std::cout << n << ", ";
+    }
+    std::cout << '\n';
+
+    auto iter{ vec.begin() }; // получаем отератор\
+    
+    // получаем эдемент, на который указывает элемент
+    std::cout << *iter << '\n'; // 1
+
+    // изменяем итератор
+    *iter = 123;
+
+    // проверяем изменения элемента
+    std::cout << vec[0]; // 123
+
+    std::cout << '\n';
+
+    for (int i = 0; i < vec.size(); i++)
+    {
+        std::cout << vec[i] << ", ";
+    }
+    std::cout << '\n';
+
+    for (int n : vec)
+    {
+        std::cout << n << ", ";
+    }
+}
+
